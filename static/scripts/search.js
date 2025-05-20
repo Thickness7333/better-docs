a6f9017a0e2bfd401f7f473e461d1c2bc201a302
p.tymoschtschuk <p.tymoschtschuk@spedion.de>
2025-05-20 10:22:05 +0200

    Improved searchperformance (code still looking a bit messy)

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,39 +1,95 @@
-(function() {
-  const input = document.querySelector('#search')
-  const targets = [ ...document.querySelectorAll('#sidebarNav li')]
-  input.addEventListener('keyup', () => {
-    // loop over each targets and hide the not corresponding ones
-    targets.forEach(target => {
-      if (!target.innerText.toLowerCase().includes(input.value.toLowerCase())) {
-        target.style.display = 'none'
+let debouncedSearch;
 
-        /**
-         * Detects an empty list
-         * Remove the list and the list's title if the list is not displayed
-         */
-        const list = [...target.parentNode.childNodes].filter( elem => elem.style.display !== 'none')
+/**
+ * @private
+ * @param {NodeList} searchTargets 
+ * @returns {Array.<Object>}
+ */
+function getDefaultSearchResults(searchTargets) {
+  const defaultSearchResults = [];
 
-        if (!list.length) {
-          target.parentNode.style.display = 'none'
-          target.parentNode.previousSibling.style.display = 'none'
-        }
-
-        /**
-         * Detects empty category
-         * Remove the entire category if no item is displayed
-         */
-        const category = [...target.parentNode.parentNode.childNodes]
-          .filter( elem => elem.tagName !== 'H2' && elem.style.display !== 'none')
-
-        if (!category.length) {
-          target.parentNode.parentNode.style.display = 'none'
-        }
-      } else {
-        target.parentNode.style.display = 'block'
-        target.parentNode.previousSibling.style.display = 'block'
-        target.parentNode.parentNode.style.display = 'block'
-        target.style.display = 'block'
-      }
-    })
+  searchTargets.forEach((traget) => {
+    const targetAsSearchResultObject = { item: traget };
+    defaultSearchResults.push(targetAsSearchResultObject);
   })
-})()
+
+  return defaultSearchResults;
+}
+
+/**
+ * @param {Fuse} fuse 
+ * @param {string} searchPattern 
+ * @param {Array.<Object>} searchTargets 
+ * @param {Array.<Object>} defaultSearchResults 
+ */
+function search(fuse, searchPattern, searchTargets, defaultSearchResults) {
+  let results = [];
+
+  if (searchPattern.length > 0) {
+    results = fuse.search(searchPattern);
+  } else {
+    results = defaultSearchResults;
+  }
+
+  searchTargets.forEach(target => {
+    const targetMatchingResult = results.find((result) => {return result.item === target});
+    if (!targetMatchingResult) {
+      target.style.display = 'none'
+
+      /**
+       * Detects an empty list
+       * Remove the list and the list's title if the list is not displayed
+       */
+      const list = [...target.parentNode.childNodes].filter( elem => elem.style.display !== 'none')
+
+      if (!list.length) {
+        target.parentNode.style.display = 'none'
+        target.parentNode.previousSibling.style.display = 'none'
+      }
+
+      /**
+       * Detects empty category
+       * Remove the entire category if no item is displayed
+       */
+      const category = [...target.parentNode.parentNode.childNodes]
+        .filter( elem => elem.tagName !== 'H2' && elem.style.display !== 'none')
+
+      if (!category.length) {
+        target.parentNode.parentNode.style.display = 'none'
+      }
+    } else {
+      target.parentNode.style.display = 'block'
+      target.parentNode.previousSibling.style.display = 'block'
+      target.parentNode.parentNode.style.display = 'block'
+      target.style.display = 'block'
+    }
+  })
+}
+
+function initSearch() {
+  const input = document.querySelector('#search');
+  const searchTargets = [ ...document.querySelectorAll('#sidebarNav li')];
+  const defaultSearchResults = getDefaultSearchResult(searchTargets);
+  debugger
+
+  const fuseOptions = {
+    isCaseSensitive: false,
+    includeScore: false,
+    threshold: 0.2,
+    keys: [
+      'innerText',
+    ]
+  };
+  
+  const fuse = new Fuse(searchTargets, fuseOptions);
+
+  input.addEventListener('keyup', () => {
+    clearTimeout(debouncedSearch);
+    debouncedSearch = setTimeout(() => {
+      const searchPattern = input.value;
+      search(fuse, searchPattern, searchTargets, defaultSearchResults);
+    }, 500);
+  })
+}
+  
+initSearch();
